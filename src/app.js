9c0407d8e750a40273a13f1832759e4edc7fb3ba
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 13:12:42 +0530

    added routes

--- src/app.js
+++ src/app.js
@@ -3,9 +3,17 @@
 const express = require('express');
 const app = express();
 
-app.get('/user/:userId/:name/:password', (req, res) => {
+app.get('/user/:userId/books/:bookId', (req, res) => {
   console.log(req.params);
   res.send({ firstName: 'Shubhangi', lastName: 'Kumbhar' });
+});
+
+app.get('/flights/:from-:to', (req, res) => {
+  res.send(req.params);
+});
+
+app.get('/platae/:genus.:species', (req, res) => {
+  res.send(req.params);
 });
 
 //Listen to the server
