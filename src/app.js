e25db0034e16369f7304996abbf65963b2248e06
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 15:10:15 +0530

    middleware chaining

--- src/app.js
+++ src/app.js
@@ -3,34 +3,17 @@
 const express = require('express');
 const app = express();
 
-// Multiple Route Handlers
+// Middleware chaining
 
-app.get('/user', [
-  (req, res, next) => {
-    console.log('Handling the route User1');
-    next();
-    // res.send('Response 1');
-  },
-  (req, res, next) => {
-    console.log('Handling the route User2');
-    // res.send('Response 2');
-    next();
-  },
-  (req, res, next) => {
-    console.log('Handling the route User3');
-    // res.send('Response 3');
-    next();
-  },
-  (req, res, next) => {
-    console.log('Handling the route User4');
-    // res.send('Response 4');
-    next();
-  },
-  (req, res, next) => {
-    console.log('Handling the route User5');
-    res.send('Response 5');
-  },
-]);
+app.get('/user', (req, res, next) => {
+  console.log('Handling the request User1');
+  next();
+});
+
+app.get('/user', (req, res, next) => {
+  console.log('Handling the request User2');
+  res.send('Response 2');
+});
 
 app.listen(3000, (req, res) => {
   console.log('Server is listening on port number 3000');
