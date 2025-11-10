2a26da5aac982168689f37535dd997116ac97a79
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 11:01:57 +0530

    updated code for hi,hello and namaste routes

--- src/app.js
+++ src/app.js
@@ -3,8 +3,30 @@
 const express = require('express');
 const app = express();
 
+//Handle Routes
+
+// handling specific test route
+app.use('/test', (req, res) => {
+  res.send('Hello from test route');
+});
+
+app.use('/namaste', (req, res) => {
+  res.send('Namaste from server');
+});
+
+app.use('/hello', (req, res) => {
+  res.send('Hello from the server');
+});
+
+app.use('/hi', (req, res) => {
+  res.send('Hi from the server');
+});
+
+// app.use((req, res) => {
+//   res.send('Hello from express js server');
+// });
+
 //Listen to the server
-app.listen(7777, (req, res) => {
-  res.send('Hello World from express server :)');
-  console.log('Server is successfully running on port number 7777');
+app.listen(3000, (req, res) => {
+  console.log('Server is successfully running on port number 3000');
 });
