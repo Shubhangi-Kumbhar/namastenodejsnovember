15cf6bf2486055ae74163709ea9339e52848a446
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 10:41:51 +0530

    created a new route

--- src/app.js
+++ src/app.js
@@ -5,6 +5,9 @@ const http = require('node:http');
 
 //Create server
 const server = http.createServer((req, res) => {
+  if (req.url === '/getSecretData') {
+    res.end('There is no secret data ');
+  }
   res.end('Hello World !!!');
 });
 
