edaf05cc0323d0b14300c817164183cae73e303c
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 10:28:53 +0530

    Creating server using htttp module

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
-console.log('First Node Js Project');
+// node js has a http module
+
+//import http module
+const http = require('node:http');
+
+//Create server
+const server = http.createServer((req, res) => {
+  res.end('Hello World !!!');
+});
+
+// Listen to the server
+server.listen(3000);
