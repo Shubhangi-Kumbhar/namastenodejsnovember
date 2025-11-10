3c27fb7ae35cd16add3bc5b8093ae6a8b7bc360f
shubhangi_kumbhar <shubhangikumbhar2010@gmail.com>
2025-11-10 12:08:22 +0530

    routes to check query params

--- src/app.js
+++ src/app.js
@@ -3,28 +3,9 @@
 const express = require('express');
 const app = express();
 
-//Handle Routes
-
-// handling specific test route
-
-// Get the data from database
-app.get('/user', (req, res) => {
+app.get('/user/:userId/:name/:password', (req, res) => {
+  console.log(req.params);
   res.send({ firstName: 'Shubhangi', lastName: 'Kumbhar' });
-});
-
-//Saving data to the database
-app.post('/user', (req, res) => {
-  res.send('Data successfully saved to database');
-});
-
-//Delete the data from database
-app.delete('/user', (req, res) => {
-  res.send('Deleted data from database');
-});
-
-//Generic route must be written at last // all the specific get , post, delete routes should be placed before generic route
-app.use('/user', (req, res) => {
-  res.send('HAHAHAHAHAHA');
 });
 
 //Listen to the server
